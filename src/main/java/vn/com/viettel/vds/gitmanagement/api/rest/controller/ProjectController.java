e99fd2fbc4bf5924a7052deaa13f1999ecc6d741
linhnq <linhnq8@viettel.com.vn>
2024-05-14 17:30:14 +0700

    add env

--- src/main/java/vn/com/viettel/vds/gitmanagement/api/rest/controller/ProjectController.java
+++ src/main/java/vn/com/viettel/vds/gitmanagement/api/rest/controller/ProjectController.java
@@ -1,25 +1,29 @@
 package vn.com.viettel.vds.gitmanagement.api.rest.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
-import retrofit2.Call;
+
+import org.springframework.web.bind.annotation.*;
 import vn.com.viettel.vds.gitmanagement.application.dto.request.git.ProjectReq;
 import vn.com.viettel.vds.gitmanagement.application.service.impl.ProjectService;
 import vn.com.viettel.vds.gitmanagement.infrastructure.entity.Project;
 
+
+
 @RestController
+@RequestMapping("/projects")
 public class ProjectController {
+    private final ProjectService gitLabService;
 
     @Autowired
-    private ProjectService projectService;
-
-    @PostMapping("/projects")
-    public void createProject(@RequestHeader("PRIVATE-TOKEN") String token, @RequestBody ProjectReq projectReq) {
-        projectService.createProject(token, projectReq);
+    public ProjectController(ProjectService gitLabService) {
+        this.gitLabService = gitLabService;
     }
 
+    @PostMapping
+    public Project createProject(@RequestHeader("Private-Token") String token, @RequestBody ProjectReq projectRequest) {
+        return gitLabService.createAndSaveProject(token, projectRequest);
+    }
 }
--- src/main/java/vn/com/viettel/vds/gitmanagement/application/service/impl/ProjectService.java
+++ src/main/java/vn/com/viettel/vds/gitmanagement/application/service/impl/ProjectService.java
@@ -1,13 +1,12 @@
 package vn.com.viettel.vds.gitmanagement.application.service.impl;
 
 
-import org.eclipse.jgit.api.errors.GitAPIException;
 import org.springframework.beans.factory.annotation.Value;
 import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 import vn.com.viettel.vds.gitmanagement.application.dto.request.git.ProjectReq;
 import vn.com.viettel.vds.gitmanagement.application.service.GitLabApiService;
 import vn.com.viettel.vds.gitmanagement.infrastructure.entity.Project;
@@ -18,51 +17,27 @@ import java.io.IOException;
 @Service
 public class ProjectService {
 
-    @Value("${gitlab.private-token}")
-    private String PRIVATE_TOKEN ;
-
-    @Value("${gitlab.url}")
-    private String BASE_REPO_URL;
-
-    @Value("${source.folder}")
-    private String SOURCE_FOLDER;
+    private static final String BASE_URL = "https://gitlab.orc.com/api/v4/";
+    private final ProjectRepo projectRepository;
+    private final GitLabApiService gitLabService;
 
     @Autowired
-    private ProjectRepo projectRepo;
+    public ProjectService(ProjectRepo projectRepository) {
+        this.projectRepository = projectRepository;
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        gitLabService = retrofit.create(GitLabApiService.class);
+    }
 
-    @Autowired
-    private GitLabApiService gitLabApiService;
-
-    @Autowired
-    private GitService gitService;
-
-    public Project createProject(String token, ProjectReq projectReq) {
-//        Call<Project> call = gitLabApiService.createProject(token, projectReq);
-//        call.enqueue(new Callback<Project>() {
-//            @Override
-//            public void onResponse(Call<Project> call, Response<Project> response) {
-//                if (response.isSuccessful()) {
-//                    Project project = response.body();
-//                    try {
-//                        gitService.cloneRepository(project.getHttpUrl(), SOURCE_FOLDER + project.getName());
-//                    } catch (GitAPIException e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//            }
-//
-//            @Override
-//            public void onFailure(Call<Project> call, Throwable t) {
-//                System.out.println("Failed to create project");
-//            }
-//        });
-//        return null;
-        Call<Project> call = gitLabApiService.createProject(token, projectReq);
+    public Project createAndSaveProject(String token, ProjectReq projectRequest) {
+        Call<Project> call = gitLabService.createProject(token, projectRequest);
         try {
             retrofit2.Response<Project> response = call.execute();
             if (response.isSuccessful()) {
                 Project project = response.body();
-                return projectRepo.save(project);
+                return projectRepository.save(project);
             } else {
                 // Handle error
                 throw new RuntimeException("Failed to create project: " + response.errorBody().string());
@@ -72,17 +47,4 @@ public class ProjectService {
             throw new RuntimeException("Failed to create project", e);
         }
     }
-
-    public Project saveProject(Project project) {
-        return projectRepo.save(project);
-    }
-//
-//    public String getProjectName(Long id) {
-//        return projectRepo.findById(id).get().getName();
-//    }
-
-    public void deleteProject(Long id) {
-        projectRepo.deleteById(id);
-    }
-
 }
